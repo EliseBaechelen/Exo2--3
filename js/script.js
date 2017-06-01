3156d636b630836f2d73677e98566824631ab0d1
Drey13009 <guitteaud.audrey@gmail.com>
2017-06-01 15:35:13 +0200

    Modif js

--- js/script.js
+++ js/script.js
@@ -4,6 +4,6 @@ Apparition img
 Envoi sur le cloud
 Juin 2017*/
 
-document.getElementById('#bouton').onclick = function(){
+document.getElementById('bouton').onclick = function(){
 	document.getElementsByTagName('p').style.display = 'none';
 };
